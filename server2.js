27b3b18bb542ba8b6b69bc303901c5962a5b1483
javisth <javisth@gmail.com>
2019-09-03 16:37:45 +0530

    add server code

--- server2.js
+++ server2.js
@@ -1,24 +1,26 @@
 var express = require("express");
 var body_parser = require("body-parser");
+var path = require("path");
 var app = express();
 
 app.use(body_parser.urlencoded({extended:false}));
 app.use(body_parser.json());
 
-app.post("/",function(req,res){
+app.post("/form-posted",function(req,res){
 	
 	res.send("Hello!"+req.body.name);
 	res.end();
 });
-/*
 
 
 
-app.get("/world", function(req,res){
-	res.send("World!");
+
+app.get("/", function(req,res){
+	/*res.sendFile("/Users/javisth/node-app2/azad.html");*/
+	res.render("form.html");
 	res.end();
 });
-*/
+
 app.listen(1337);
 console.log('Server running at http://127.0.0.1:1337/');
 
